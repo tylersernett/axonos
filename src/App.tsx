e5b5b730a29b4d48dd0e3cd3345ddfc51aa9dcc4
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-11-03 15:52:47 -0700

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css'
+import About from './components/About'
 import Home from './components/Home'
+import { Routes, Route, Link, useMatch, useNavigate, BrowserRouter } from 'react-router-dom'
+import NavBar from './components/Navbar'
+import Procedures from './components/Procedures'
+
 
 function App() {
 
   return (
     <>
-      <Home/>
+
+      <BrowserRouter >
+        <NavBar />
+        {/* <Home /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="procedures" element={<Procedures />} />
+          {/* <Route path="/create" element={<CreateNew addNew={addNew} setNotification={setNotification} />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
