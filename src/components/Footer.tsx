816fec4d4131063580eed0e5d19206f4f9f08b09
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-11-06 17:45:46 -0800

    Extract Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <ul className="icons">
+        <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
+        <li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
+        <li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
+        {/* <li><a href="#" className="icon brands fa-github"><span className="label">Github</span></a></li> */}
+        {/* <li><a href="#" className="icon brands fa-dribbble"><span className="label">Dribbble</span></a></li> */}
+        {/* <li><a href="#" className="icon brands fa-google-plus"><span className="label">Google+</span></a></li> */}
+      </ul>
+      <ul className="copyright">
+        <li>&copy; Axonos. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -105,23 +105,11 @@ const Home = () => {
 
         </section>
 
-        {/* <!-- Footer --> */}
-        <footer id="footer">
-          <ul className="icons">
-            <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon brands fa-github"><span className="label">Github</span></a></li>
-            <li><a href="#" className="icon brands fa-dribbble"><span className="label">Dribbble</span></a></li>
-            <li><a href="#" className="icon brands fa-google-plus"><span className="label">Google+</span></a></li>
-          </ul>
-          <ul className="copyright">
-            <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-          </ul>
-        </footer>
+        
 
-      </div>
-    </div>
+       </div>
+    {/* </div> */}
+    </>
   )
 }
 
