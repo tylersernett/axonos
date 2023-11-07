e4a038d469f65fdcb65a615f2d4bcbbbc2efaa41
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-11-06 18:03:11 -0800

    Add permanent Banner & Footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Home from './components/Home'
 import { Routes, Route, Link, useMatch, useNavigate, BrowserRouter } from 'react-router-dom'
 import NavBar from './components/Navbar'
 import Procedures from './components/Procedures'
+import Contact from './components/Contact'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -13,13 +16,16 @@ function App() {
 
       <BrowserRouter >
         <NavBar />
+        <Banner />
         {/* <Home /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="procedures" element={<Procedures />} />
+          <Route path="contact" element={<Contact />} />
           {/* <Route path="/create" element={<CreateNew addNew={addNew} setNotification={setNotification} />} /> */}
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
