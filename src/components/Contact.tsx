f61092da8342a8e2e98a0080b200f3f3c29debc8
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-11-10 02:23:07 -0500

    Add contact page

---
+++ src/components/Contact.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import p1 from '../images/greenmodel.png'
+import ContactForm from './ContactForm'
+
+const Contact = () => {
+  return (
+    <div id="page-wrapper">
+
+      <section id="main" className="container moveup ">
+        <section id="main" className=" box special">
+          <header>
+            <h2>Contact</h2>
+            <p>Reach out online or visit us in person</p>
+          </header>
+          <div className="box">
+            <span className="image featured"><img src={p1} alt="" style={{ filter: 'grayscale(50%)' }} /></span>
+            <div className='contact-flex' style={{ display: 'flex',  }}>
+              <div style={{ flex: '1 1 50%', margin: '1em' }}>
+                <ContactForm />
+              </div>
+              <hr/>
+              <div style={{ flex: '1 1 50%', margin: '1em' }}>
+                <h3>Address</h3>
+                <iframe width='100%' height='80%' style={{ border: 0, minHeight:'300px' }} allowFullScreen={false} loading="lazy" referrerPolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3355.8849963541834!2d-97.328424!3d32.742244299999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e711294d5df0b%3A0x6f937f80b9ee35a3!2s209%20St%20Louis%20Ave%20%23100%2C%20Fort%20Worth%2C%20TX%2076104!5e0!3m2!1sen!2sus!4v1699201243834!5m2!1sen!2sus" ></iframe>
+                <p>209 St Louis Ave #100
+                  <br />Fort Worth, TX 76104
+                  <br />(817)-XXX-XXXX
+                </p>
+              </div>
+
+            </div>
+          </div>
+        </section>
+      </section>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,133 @@
+import { React, useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+// import { shades } from '../theme';
+
+const ContactForm = () => {
+  const [isSent, setIsSent] = useState(false)
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required('required'),
+    email: Yup.string().email('invalid email').required('required'),
+    subject: Yup.string().required('required'),
+    message: Yup.string().required('required')
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      subject: '',
+      message: ''
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      fetch('https://formsubmit.co/ajax/7793f1e72a9025f1888edee332bccdef', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(values),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          //successful response:
+          formik.resetForm();
+          setIsSent(true)
+          //return response.text();
+        })
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error('There was a problem with the form submission:', error);
+        });
+    },
+  });
+
+  return (
+    <Box>
+      <h3 id='contactForm'>Email Form</h3>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          name='name'
+          label='Name'
+          fullWidth
+          margin='dense'
+          sx={{mt:'0'}}
+          variant='outlined'
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+        />
+        <TextField
+          name='email'
+          label='Email'
+          fullWidth
+          margin='dense'
+          variant='outlined'
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          name='subject'
+          label='Subject'
+          fullWidth
+          margin='dense'
+          variant='outlined'
+          value={formik.values.subject}
+          onChange={formik.handleChange}
+          error={formik.touched.subject && Boolean(formik.errors.subject)}
+          helperText={formik.touched.subject && formik.errors.subject}
+        />
+        <TextField
+          name='message'
+          label='Message'
+          fullWidth
+          margin='dense'
+          multiline
+          rows={4}
+          variant='outlined'
+          sx={{ backgroundColor: '#f8f8f8' }}
+          value={formik.values.message}
+          onChange={formik.handleChange}
+          error={formik.touched.message && Boolean(formik.errors.message)}
+          helperText={formik.touched.message && formik.errors.message}
+        />
+        <Button
+          type='submit'
+          variant='contained'
+          sx={{ mt: '8px' , width:'100%', '@media (max-width: 736px)': { width: '100%' }}}
+          disabled={formik.isSubmitting}
+          fullWidth={!isNonMobile}
+        >
+          Submit
+        </Button>
+      </form>
+
+      {/* display the following confirmation once server response is received & isSent is true */}
+      {isSent && (
+        <Typography variant="subtitle1" color="white">
+          Your message has been received!
+          <br />
+          A member of our team will reach out soon.
+        </Typography>
+      )}
+
+    </Box>
+  );
+};
+
+export default ContactForm;
