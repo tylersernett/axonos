75abf7784e1a675293a645263d38f4f28208f9e4
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-11-10 02:23:34 -0500

    Convert buttons to HashLinks

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,25 @@
+import { Box, Button, Typography, } from "@mui/material"
+import { NavLink } from "react-router-dom"
+import { HashLink } from 'react-router-hash-link';
+
 const Banner = () => {
   return (
     <div className="landing is-preload">
 
       <section id="banner">
-        <h2>Axonos</h2>
-        <p>Spinal specialists serving the Dallas community</p>
-        <ul className="actions special">
-          <li><a href="#" className="button primary">Book Appointment</a></li>
-          <li><a href="#" className="button">Learn More</a></li>
-        </ul>
+        {/* <h2>Axonos</h2> */}
+        <Typography variant='h1' color='white'>Axonos</Typography>
+        <p>Spinal specialists serving the Fort Worth community</p>
+        <Box >
+          <HashLink smooth to={'/contact#contactForm'} style={{ textDecoration: 'none', color: 'white' }} tabIndex={-1}>
+            <Button variant="contained" sx={{ '@media (max-width: 415px)': { width: '100%' }}}>Book Appointment</Button>
+          </HashLink>
+          <HashLink smooth to={'/about#about'} tabIndex={-1}>
+            <Button variant="outlined" sx={{ color: 'white', borderColor: '#e5e5e5', '@media (max-width: 415px)': { width: '100%' } }}>
+              Learn More
+            </Button>
+          </HashLink>
+        </Box>
       </section>
     </div>
   )
