a84cb5f89009ca7e89d472512684a4fd545eda9f
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-11-10 02:23:55 -0500

    Add doctor image

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,45 +1,58 @@
 // import { Typography } from '@mui/material'
 import React from 'react'
 import p1 from '../images/spinefish.png'
+import headshot from '../images/headshot.jpg'
+import { Box } from '@mui/material'
 
 const About = () => {
   return (
     <div id="page-wrapper">
       <section id="main" className="container moveup">
 
-{/* ??? delete "special" classname? */}
+        {/* ??? delete "special" classname? */}
         <section id="main" className="box special">
           {/* <section id="main" className="container"> */}
           <header>
-            <h2>About</h2>
-            <p >Read more about our clinic and expertise</p>
+            <h2 id='about'>About</h2>
+            <p>Read more about our clinic and expertise</p>
           </header>
           {/* <div className="box"> */}
           <div className="box" >
             <span className="image featured">
               {/* <img src={p1} alt="" style={{ maxHeight: '290px', overflow: 'hidden'  }} /> */}
-              <div style={{  overflow: 'hidden' }}>
-                <img src={p1} alt=""  style={{filter: 'grayscale(0%)'}} />
-              </div>
+              {/* <div style={{ overflow: 'hidden' }}> */}
+              <img src={p1} alt="" style={{ filter: 'grayscale(0%)' }} />
+              {/* </div> */}
             </span>
-            <h3>Our Physician</h3>
-            <div style={{textAlign: 'left'}}>
-            <p>
-              Riley Johnson, DO, is a fellowship-trained interventional pain management physician.
-            </p>
-            <p>
-              A Texas native, he attended Churchill High School in San Antonio. He earned an undergraduate degree in Civil Engineering at the University of Texas at Austin. He went on to study medicine at the University of North Texas Health Science Center in Fort Worth, where he earned a Master's Degree and his Doctorate.
-            </p>
-            <p>
-              He completed his Physical Medicine and Rehabilitation residency training at Baylor University Medical Center in Dallas, Texas, where he was named outstanding resident of the year in his second year of training.
-            </p>
-            <p>
-              He went on to complete a fellowship in Interventional Pain Management at Virginia Commonwealth University in Richmond, Virginia.
-            </p>
-            <p>
-              Dr. Johnson has interests in neuromodulation and regenerative medicine. He enjoys spending time with his wife and daughter, weight lifting, playing blues guitar, and enjoying live music.
-            </p>
-            </div>
+            <h3 style={{ marginBottom: '1.5em' }}>Our Physician</h3>
+            <Box display={'flex'} gap={'1em'} className='contact-flex'>
+
+              <div style={{ flex: '1 1 50%', }}>
+                <div id='physician-text' style={{ textAlign: 'left' }}>
+                  <p>
+                    Riley Johnson, DO, is a fellowship-trained interventional pain management physician.
+                  </p>
+                  <p>
+                    A Texas native, he attended Churchill High School in San Antonio. He earned an undergraduate degree in Civil Engineering at the University of Texas at Austin. He went on to study medicine at the University of North Texas Health Science Center in Fort Worth, where he earned a Master's Degree and his Doctorate.
+                  </p>
+                  <p>
+                    He completed his Physical Medicine and Rehabilitation residency training at Baylor University Medical Center in Dallas, Texas, where he was named outstanding resident of the year in his second year of training.
+                  </p>
+                  <p>
+                    He went on to complete a fellowship in Interventional Pain Management at Virginia Commonwealth University in Richmond, Virginia.
+                  </p>
+                  <p>
+                    Dr. Johnson has interests in neuromodulation and regenerative medicine. He enjoys spending time with his wife and daughter, weight lifting, playing blues guitar, and enjoying live music.
+                  </p>
+                </div>
+              </div>
+
+              <div style={{ flex: '1 1 50%',}}>
+                <img src={headshot} alt="" style={{ filter: 'grayscale(0%)', width: '100%', transform: 'scaleX(-1)' }} />
+              </div>
+
+            </Box>
+
             {/* <div className="row">
               <div className="row-6 row-12-mobilep">
                 <h3>And now a subheading</h3>
