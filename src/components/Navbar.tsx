6280806f60978c7a9fbbd5b62a887a0c6fb43026
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-11-10 02:22:40 -0500

    Improve responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,10 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem, Box, Button } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Typography, Menu, MenuItem, Box, Button, useMediaQuery } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 // import { Link } from 'react-router-dom'; // If you're using React Router
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 // import useMediaQuery from '../hooks/useMediaQuery';
 import { NavLink } from 'react-router-dom';
-
-const Link = ({ page, selectedPage }) => {
-  const lowerCasePage = page.toLowerCase();
-  return (
-    <AnchorLink className={`${selectedPage === lowerCasePage ? "text-yellow underline underline-offset-2" : ""}
-       hover:text-yellow transition duration-500`}
-      href={`#${lowerCasePage}`}
-    // onClick={() => {
-    //   setIsMenuOpen(false)
-    // }}
-    >
-      {page}
-    </AnchorLink>
-  )
-}
 
 const NavBar = () => {
   // const isAboveSmallScreens = useMediaQuery('(min-width: 768px)');
@@ -33,38 +18,53 @@ const NavBar = () => {
     setMenuAnchorEl(null);
   };
 
-  const navPages = ["Home", "About", "Diagnoses & Symptoms", "Procedures", "Contact"];
-  const navObj = [{ text: "Home", link: '/' }, { text: "About", link: '/about' }, { text: "Diagnoses & Symptoms", link: '/diagnoses' }, { text: "Procedures", link: '/procedures' }, { text: "Contact", link: '/contact' }];
+  // const navObj = [{ text: "Home", link: '/' }, { text: "About", link: '/about' }, { text: "Diagnoses & Symptoms", link: '/diagnoses' }, { text: "Procedures", link: '/procedures' }, { text: "Contact", link: '/contact' }];
+  const navObj = [{ text: "Home", link: '/' }, { text: "About", link: '/about' }, { text: "Diagnoses & Procedures", link: '/procedures' }, { text: "Contact", link: '/contact' }];
   let selectedPage = null;
+
+  const isSmallScreen = useMediaQuery('(max-width:736px)');
 
   return (
     <Box sx={{ display: 'flex' }}>
 
-      <AppBar >
-        <Toolbar>
+      {/* linear-gradient( top color, bottom color )*/}
+      {/* <AppBar  style={{ background: 'linear-gradient(rgba(0, 150, 136, 1.0), rgba(0, 150, 136, 0.8))'}}> */}
+      <AppBar position='absolute'
+        style={{
+          maxHeight: '60px',
+          // background: 'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1))',
+          // background: 'linear-gradient(rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.0))',
+          background: 'rgba(0, 0, 0, 0.0)',
+          boxShadow: 'none',
+        }}
+      >
+        <Toolbar >
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={handleMenuClick}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            size='large'
+            sx={{ mr: 2, display: isSmallScreen ? 'flex' : 'none' }}
           >
             <MenuIcon />
           </IconButton>
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            // sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: isSmallScreen ? 'none' : 'block' }}
           >
             AXONOS
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+          <Box sx={{ display: isSmallScreen ? 'none' : 'flex' }}>
             {navObj.map((page) => (
               // <Link key={page} page={page} selectedPage={selectedPage} />
               <NavLink key={page.text} to={page.link} style={{ textDecoration: 'none', color: 'white' }} tabIndex={-1}>
                 <Button
+                  variant='contained'
                   onClick={handleMenuClose} //to ensure scroll doesn't get locked if user transitions from small to large window
-                  sx={{ my: 2, color: 'white', display: 'block', backgroundColor: 'transparent' }}
+                  sx={{ color: 'white', backgroundColor: 'transparent', boxShadow: 'none' }}
                 >
                   {page.text}
                 </Button>
@@ -79,27 +79,11 @@ const NavBar = () => {
           >
             {navObj.map((page) => (
               <MenuItem key={page.text} onClick={handleMenuClose}>
-                <NavLink to={page.link}  tabIndex={-1}>
+                <NavLink to={page.link} tabIndex={-1}>
                   {page.text}
                 </NavLink>
-                {/* <Link page={page} selectedPage={selectedPage} /> */}
               </MenuItem>
             ))}
-            {/* <MenuItem onClick={handleMenuClose}>
-              <Link to="/">Home</Link>
-            </MenuItem>
-            <MenuItem onClick={handleMenuClose}>
-              <Link to="/about">About</Link>
-            </MenuItem>
-            <MenuItem onClick={handleMenuClose}>
-              <Link to="/diagnoses">Diagnoses & Symptoms</Link>
-            </MenuItem>
-            <MenuItem onClick={handleMenuClose}>
-              <Link to="/procedures">Procedures</Link>
-            </MenuItem>
-            <MenuItem onClick={handleMenuClose}>
-              <Link to="/contact">Contact</Link>
-            </MenuItem> */}
           </Menu>
         </Toolbar>
       </AppBar>
