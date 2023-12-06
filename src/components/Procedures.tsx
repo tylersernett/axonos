32effba372652e8936f84e62a2cf230d9410d435
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-12-06 00:25:31 -0500

    Update capitalization

--- src/components/Procedures.tsx
+++ src/components/Procedures.tsx
@@ -1,48 +1,48 @@
 import p1 from '../images/multi-xray.png'
 
 const Procedures = () => {
-  const diagText = `osteoarthritis
-  chronic pain syndrome
+  const diagText = `Osteoarthritis
+  Chronic pain syndrome
   CRPS
-  diabetic neuropathy
-  degenerative disc disease
-  post-laminectomy syndrome
-  compression fracture
-  trigger points
-  lumbar and cervical radiculopathy
-  peripheral neuropathy
-  spondylosis
-  neuralgia
-  migraines
-  spasticity
-  piriformis syndrome
-  muscle spasm
-  sciatica
-  meralgia paresthetica
-  paresthesia and numbness
-  neck and back pain
-  knee, hip, and shoulder pain
-  rib pain
-  tailbone pain
-  phantom limb pain
-  pain following stroke, spinal cord injury or brain injury`
+  Diabetic neuropathy
+  Degenerative disc disease
+  Post-laminectomy syndrome
+  Compression fracture
+  Trigger points
+  Lumbar and cervical radiculopathy
+  Peripheral neuropathy
+  Spondylosis
+  Neuralgia
+  Migraines
+  Spasticity
+  Piriformis syndrome
+  Muscle spasm
+  Sciatica
+  Meralgia paresthetica
+  Paresthesia and numbness
+  Neck and back pain
+  Knee, hip, and shoulder pain
+  Rib pain
+  Tailbone pain
+  Phantom limb pain
+  Pain following stroke, spinal cord injury or brain injury`
 
-  const procText = `Transforaminal Epidural Steroid Injection
-  Interlaminar Epidural Steroid Injection
-  Peripheral Joint Injection
-  Facet Joint Injection
-  Trigger Point Injection
-  Stellate Ganglion Block
-  Sympathetic Block
-  Radiofrequency Ablation
-  Medial Branch Block
-  Sacral Lateral Branch Block
-  Peripheral Nerve Block
-  Basivertebral Nerve Ablation
-  Spinal Cord Stimulator
-  Intrathecal Pump
+  const procText = `Transforminal epidural steroid injection
+  Interlaminar epidural steroid injection
+  Peripheral joint injection
+  Facet joint injection
+  Trigger point injection
+  Stellate ganglion block
+  Sympathetic block
+  Radiofrequency ablation
+  Medial branch block
+  Sacral lateral branch block
+  Peripheral nerve block
+  Basivertebral nerve ablation
+  Spinal cord stimulator
+  Intrathecal pump
   Kyphoplasty
-  Regenerative Medicine Procedures`
+  Regenerative medicine procedures`
 
   const style: React.CSSProperties = {
     whiteSpace: 'pre-line',
