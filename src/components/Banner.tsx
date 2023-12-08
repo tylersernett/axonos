151480642298ab4ad498556235574cee8266d084
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-12-07 22:25:12 -0500

    Fix centering of h1 and buttons

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,11 +8,13 @@ const Banner = () => {
     fontFamily: 'Galvji',
     fontSize: '1em',
     letterSpacing: "0.5em",
+    paddingLeft: '0.5em', //whem using letterSpacing, add paddingLeft to keep centered (1st char is not spaced)
   };
   const h1Style: React.CSSProperties = {
     color: "white",
     fontFamily: 'Galvji',
     letterSpacing: "0.2em",
+    paddingLeft: '0.2em',
     fontWeight: 'bold',
   };
 
@@ -20,15 +22,17 @@ const Banner = () => {
     <div className="landing is-preload">
 
       <section id="banner">
-        <Typography variant='h1' sx={{ ...h1Style }} ><Link to={'/'}>AXONOS</Link></Typography>
+        <Typography variant='h1' sx={{ ...h1Style, '@media (max-width: 415px)': { fontSize: '3em' } }} >
+          <Link to={'/'}>AXONOS</Link>
+        </Typography>
         <p style={subtitleStyle}>PAIN MANAGEMENT</p>
         <p style={{ marginLeft: '1em', marginRight: '1em' }}>Interventional pain management specialists serving the Fort Worth community</p>
         <Box >
           <HashLink smooth to={'/contact#contactForm'} style={{ textDecoration: 'none', color: 'white' }} tabIndex={-1}>
-            <Button variant="contained" sx={{ '@media (max-width: 415px)': { width: '100%' } }}>Book Appointment</Button>
+            <Button variant="contained" sx={{ width:'185px', '@media (max-width: 480px)': { width: '100%' } }}>Book Appointment</Button>
           </HashLink>
           <HashLink smooth to={'/about#about'} tabIndex={-1}>
-            <Button variant="outlined" sx={{ color: 'white', borderColor: '#e5e5e5', '@media (max-width: 415px)': { width: '100%' } }}>
+            <Button variant="outlined" sx={{ color: 'white', borderColor: '#e5e5e5', width:'185px', '@media (max-width: 480px)': { width: '100%' } }}>
               Learn More
             </Button>
           </HashLink>
