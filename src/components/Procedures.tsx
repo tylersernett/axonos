13c7bbb308a9e94ea62f4b6273aeb3346219855b
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-12-31 17:37:01 -0500

    Edit margins for consistency

--- src/components/Procedures.tsx
+++ src/components/Procedures.tsx
@@ -62,7 +62,7 @@ const Procedures = () => {
   };
 
   const columnStyle: React.CSSProperties = {
-    margin: '1em',
+    margin: '1em 0',
     flexGrow: 1,
   };
 
@@ -85,7 +85,7 @@ const Procedures = () => {
             </span>
             <div className="box" >
             <h3 style={{ marginBottom: '1.5em' }}>Our Mission</h3>
-            <div style={{ margin: '1em', textAlign:'left' }}>
+            <div style={{ margin: '0 0 1em 0', textAlign:'left' }}>
               At Axonos, we are committed to ensuring you have access to the information you need to make informed decisions about your health. Our facilities feature state of the art technology, including C-arm guidance. Whether you're seeking insights into your diagnosis or exploring treatment options, we're here to guide you through your medical journey. If you have any questions or would like to schedule a consultation, please don't hesitate to contact us. Your well-being is our priority.
             </div>
 
